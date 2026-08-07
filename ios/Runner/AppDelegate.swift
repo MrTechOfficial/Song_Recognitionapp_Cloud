548acbfe5ef51f05e07dee47ac6f937a9697b50f
MrTechOfficial <georgethomasbazos@gmail.com>
2026-08-07 17:00:02 -0400

    Fix Swift AppShortcutsBuilder syntax in AppDelegate

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -2,41 +2,6 @@ import UIKit
 import Flutter
 import AppIntents
 
-@available(iOS 16.0, *)
-struct FindSongIntent: AppIntent {
-    static var title: LocalizedStringResource = "Find Song"
-    static var description = IntentDescription("Opens Reczt and starts recognizing audio.")
-
-    static var openAppWhenRun: Bool = true
-
-    @MainActor
-    func perform() async throws -> some IntentResult {
-        if let controller = UIApplication.shared.windows.first?.rootViewController as? FlutterViewController {
-            let channel = FlutterMethodChannel(name: "com.handsfreefinder/siri", binaryMessenger: controller.binaryMessenger)
-            channel.invokeMethod("onSiriTrigger", arguments: nil)
-        }
-        return .result()
-    }
-}
-
-@available(iOS 16.0, *)
-struct AppShortcuts: AppShortcutsProvider {
-    static var appShortcuts: [AppShortcut] {
-        [
-            AppShortcut(
-                intent: FindSongIntent(),
-                phrases: [
-                    "Find song in \(.applicationName)",
-                    "Find song with \(.applicationName)",
-                    "Find song"
-                ],
-                shortTitle: "Find Song",
-                systemImageName: "music.note"
-            )
-        ]
-    }
-}
-
 @main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
@@ -44,6 +9,34 @@ struct AppShortcuts: AppShortcutsProvider {
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    return super.application(application, launchOptions: launchOptions)
   }
+}
+
+@available(iOS 16.0, *)
+struct FindSongIntent: AppIntent {
+    static var title: LocalizedStringResource = "Find Song"
+    static var openAppWhenRun: Bool = true
+
+    @MainActor
+    func perform() async throws -> some IntentResult {
+        return .result()
+    }
+}
+
+@available(iOS 16.0, *)
+struct AppShortcuts: AppShortcutsProvider {
+    static var appShortcuts: [AppShortcut] {
+        //  Notice: NO square brackets [ ] around AppShortcut(...)
+        AppShortcut(
+            intent: FindSongIntent(),
+            phrases: [
+                "Find song in \(.applicationName)",
+                "Find song with \(.applicationName)",
+                "Find song"
+            ],
+            shortTitle: "Find Song",
+            systemImageName: "music.note"
+        )
+    }
 }
